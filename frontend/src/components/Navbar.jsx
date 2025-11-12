9959dd57a569134bd5df71979fd120c32cb8588b
Gursharen Kaur Suri <150019277+GursharenKaur@users.noreply.github.com>
2025-11-12 17:46:51 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const handleSubmit = async (e) => {
   }
 
   try {
-    const response = await fetch("http://localhost:6754/send-feedback", {
+    const response = await fetch("https://casequest-25.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, message }),
